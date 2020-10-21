7f1fb2147bb41ce7586fcac1a356defe0a18c1c1
guoxiang2 <guoxiang2@yy.com>
2020-10-21 15:16:22 +0800

    someone peek me

--- noncopyable.h
+++ noncopyable.h
@@ -3,21 +3,33 @@
 
 ///
 /// hasaki::base::noncopyable likes boost::noncopyable.
-/// 
+///
 namespace hasaki
 {
     namespace base
     {
         class noncopyable
         {
-            #if __cplusplus < 201103L
-            private:
-                noncopyable(const noncopyable &);
-                noncopyable& operator=(const noncopyable&);
-            #else
-                noncopyable() = delete;
-                noncopyable& operator=(const noncopyable &) = delete;
-            #endif
+// constructor and destrutor
+#if __cplusplus < 201103L
+            noncopyable()
+            {
+            }
+            ~noncopyable() {}
+#else
+            constexpr noncopyable() = default;
+            ~noncopyable() = default;
+#endif
+
+// disable copy and assign
+#if __cplusplus < 201103L
+        private:
+            noncopyable(const noncopyable &);
+            noncopyable &operator=(const noncopyable &);
+#else
+            noncopyable() = delete;
+            noncopyable &operator=(const noncopyable &) = delete;
+#endif
         };
 
     } // namespace  base
