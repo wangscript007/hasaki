1561d6d41c7a7ca4bc5aac5a03a83f6f5c7b49e6
guoxiang2 <guoxiang2@yy.com>
2020-10-24 11:27:33 +0800

    ipv4&ipv6的支持

--- inet_address.h
+++ inet_address.h
@@ -1,16 +1,39 @@
 #ifndef __HASAKI_INET_ADDRESS_H__
 #define __HASAKI_INET_ADDRESS_H__
 
+#include <cstring>
+#include <netinet/in.h>
+
 namespace hasaki {
 namespace net {
 
+// Generic InetAddress.
 class InetAddress {};
 
-// For IpV4
-class Inet4Address {};
+// InetAdress for IPV4
+class Inet4Address : public InetAddress {
+public:
+    Inet4Address() {
+        sa_family_t ipv4Val = AF_INET;
+        memcpy(&sockAddr__.sin_family, &ipv4Val, sizeof(sa_family_t));
+    }
 
-// For IpV6
-class Inet6Address {};
+private:
+    struct sockaddr_in sockAddr__;
+};
+
+// InetAddress for IPV6
+class Inet6Address {
+
+public:
+    Inet6Address() {
+        sa_family_t ipv6Val = AF_INET6;
+        memcpy(&sockAddr__.sin_family, &ipv6Val, sizeof(sa_family_t));
+    }
+
+private:
+    struct sockaddr_in sockAddr__;
+};
 
 } // namespace net
 } // namespace hasaki
