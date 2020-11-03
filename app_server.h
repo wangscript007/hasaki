bf1e8907cca89338c5056d2e6af3280f362912d7
guoxiang2 <guoxiang2@yy.com>
2020-11-03 15:53:04 +0800

    bytebuffer must be noncopy and assign

--- app_server.h
+++ app_server.h
@@ -1,13 +1,15 @@
 #ifndef __HASAKI_APP_SERVER_H__
 #define __HASAKI_APP_SERVER_H__
 
+#include <memory>
+
 namespace hasaki {
 namespace net {
 
 /// <summary>
 /// 
 /// </summary>
-class AppServer {
+class AppServer : public std::enable_shared_from_this<AppServer> {
 public:
     AppServer() = default;
     virtual ~AppServer() = default;
